670c92d5192cf8467cb507f7cb2b048b3c7090c2
Nicolas Goudry <goudry.nicolas@gmail.com>
2017-02-05 01:34:46 +0100

    Update webpack to v2

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,49 @@
-var webpack = require('webpack');
-//import webpack from 'webpack';
+const path = require('path')
 
+// Webpack 1
+/*
 module.exports = {
   entry: './src/index.js',
-  externals:{
-    "react": "umd react"
+  externals: {
+    react: 'umd react'
   },
-  output: { path: __dirname + '/dist', filename: 'social-login.js', libraryTarget: 'umd' },
-  module: {
-    loaders: [
-      {
-        test: /.jsx?$/,
-        loader: 'babel-loader',
-        exclude: /node_modules/,
-        query: {
-          presets: ['es2015', 'react']
-        }
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'social-login.js',
+    libraryTarget: 'umd'
+  },
+  module: {
+    loaders: [{
+      test: /\.(js|jsx)$/,
+      exclude: /node_modules/,
+      loader: 'babel',
+      query: {
+        presets: ['es2015', 'react', 'stage-2']
       }
-    ]
-  },
-};
+    }]
+  }
+}
+*/
+
+// Webpack 2
+module.exports = {
+  entry: './src/index.js',
+  externals: {
+    react: 'umd react'
+  },
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'social-login.js',
+    libraryTarget: 'umd'
+  },
+  module: {
+    rules: [{
+      test: /\.(js|jsx)$/,
+      exclude: /node_modules/,
+      loader: 'babel-loader',
+      options: {
+        presets: ['es2015', 'react', 'stage-2']
+      }
+    }]
+  }
+}
