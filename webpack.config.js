8a132a0c7beb266b637960c384c683ce0c37e813
Nicolas Goudry <goudry.nicolas@gmail.com>
2017-02-05 01:43:03 +0100

    Remove useless old webpack 1 config and force node 6+

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,5 @@
 const path = require('path')
 
-// Webpack 1
-/*
-module.exports = {
-  entry: './src/index.js',
-  externals: {
-    react: 'umd react'
-  },
-  output: {
-    path: path.join(__dirname, 'dist'),
-    filename: 'social-login.js',
-    libraryTarget: 'umd'
-  },
-  module: {
-    loaders: [{
-      test: /\.(js|jsx)$/,
-      exclude: /node_modules/,
-      loader: 'babel',
-      query: {
-        presets: ['es2015', 'react', 'stage-2']
-      }
-    }]
-  }
-}
-*/
-
-// Webpack 2
 module.exports = {
   entry: './src/index.js',
   externals: {
