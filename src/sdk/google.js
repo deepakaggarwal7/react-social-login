1279736251f74bd472bdd353878363ef1b105152
Nicolas Goudry <goudry.nicolas@gmail.com>
2017-07-16 03:11:27 +0200

    Upgrade Google SDK helper

--- src/sdk/google.js
+++ src/sdk/google.js
@@ -1,5 +1,6 @@
-const googleSDKLoader = (d, cid, appId, fn, err) => {
-  const js = d.createElement('script')
+const load = (appId) => new Promise((resolve, reject) => {
+  const firstJS = document.getElementsByTagName('script')[0]
+  const js = document.createElement('script')
 
   js.src = 'https://apis.google.com/js/platform.js'
   js.id = 'gapi-client'
@@ -7,18 +8,65 @@ const googleSDKLoader = (d, cid, appId, fn, err) => {
   js.onload = () => {
     window.gapi.load('auth2', () => {
       if (!window.gapi.auth2.getAuthInstance()) {
-        window.gapi.auth2.init({client_id: appId})
+        window.gapi.auth2.init({
+          client_id: appId
+        })
       }
-
-      window.gapi.auth2.getAuthInstance().attachClickHandler(cid, {}, fn, err)
     })
   }
 
-  if (document.getElementsByTagName('script').length === 0) {
-    d.appendChild(js)
+  if (!firstJS) {
+    document.appendChild(js)
   } else {
-    document.getElementsByTagName('script')[0].parentNode.appendChild(js)
+    firstJS.parentNode.appendChild(js)
+  }
+})
+
+const checkLogin = () => new Promise((resolve, reject) => {
+  const GoogleAuth = window.gapi.auth2.getAuthInstance()
+
+  if (!GoogleAuth.isSignedIn.get()) {
+    return reject()
+  }
+
+  return resolve(GoogleAuth.currentUser.get())
+})
+
+const login = () => new Promise((resolve, reject) => {
+  checkLogin()
+    .then((profile) => resolve(profile))
+    .catch(() => {
+      const GoogleAuth = window.gapi.auth2.getAuthInstance()
+
+      GoogleAuth.signIn().then(
+        () => GoogleAuth.currentUser.get(),
+        () => reject())
+    })
+})
+
+const generateUser = (response) => {
+  const profile = response.getBasicProfile()
+  const authResponse = response.getAuthResponse(true)
+
+  return {
+    profile: {
+      id: profile.getId(),
+      name: profile.getName(),
+      firstName: profile.getGivenName(),
+      lastName: profile.getFamilyName(),
+      email: profile.getEmail(),
+      profilePicURL: profile.getImageUrl()
+    },
+    token: {
+      accessToken: authResponse.access_token,
+      expiresAt: authResponse.expires_at
+    }
   }
 }
 
-export default googleSDKLoader
+export default {
+  checkLogin,
+  generateUser,
+  load,
+  login
+}
