1426c90e98e99585c090f6ece1615a6059f303dd
Nicolas Goudry <goudry.nicolas@gmail.com>
2017-07-16 03:46:47 +0200

    Update demo

--- demo/index.js
+++ demo/index.js
@@ -3,30 +3,57 @@ import ReactDOM from 'react-dom'
 
 import SocialLogin from '../src'
 
-const handleSocialLogin = (user, err) => {
+const handleSocialLogin = (user) => {
   console.log(user)
-  console.log(err)
 }
 
-const PureButton = (props) => (
-  <button {...props}>Login with Facebook</button>
-)
+const handleSocialLoginFailure = (err) => {
+  console.error(err)
+}
 
 class Button extends Component {
+  static propTypes = {
+    triggerLogin: PropTypes.func.isRequired
+  }
+
   render () {
+    const style = {
+      background: '#eee',
+      border: '1px solid black',
+      borderRadius: '3px',
+      display: 'inline-block',
+      margin: '5px',
+      padding: '10px 20px'
+    }
+
     return (
-      <button {...this.props}>Login with Facebook</button>
+      <div onClick={this.props.triggerLogin} style={style}>
+        { this.props.children }
+      </div>
     )
   }
 }
 
-const PureSocialButton = SocialLogin(PureButton)
 const SocialButton = SocialLogin(Button)
 
 ReactDOM.render(
   <div>
-    <PureSocialButton provider='facebook' appId='1724499281174401' onLoginSuccess={handleSocialLogin} />
-    <SocialButton provider='linkedin' appId='1724499281174401' onLoginSuccess={handleSocialLogin} />
+    <SocialButton
+      provider='facebook'
+      appId='1724499281174401'
+      onLoginSuccess={handleSocialLogin}
+      onLoginFailure={handleSocialLoginFailure}
+    >
+      Login with Facebook
+    </SocialButton>
+    <SocialButton
+      provider='google'
+      appId='1085669919173-lslfngv7lb6j9sr7eostmtk54mrdmhc5.apps.googleusercontent.com'
+      onLoginSuccess={handleSocialLogin}
+      onLoginFailure={handleSocialLoginFailure}
+    >
+      Login with Google
+    </SocialButton>
   </div>,
   document.getElementById('app')
 )
