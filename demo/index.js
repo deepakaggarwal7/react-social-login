9b614692ed2b0e867e58e4c60f4aa95f3ba23341
Nicolas Goudry <goudry.nicolas@gmail.com>
2017-07-27 23:54:43 +0200

    Update demo

--- demo/index.js
+++ demo/index.js
@@ -37,50 +37,91 @@ class Button extends Component {
 
 const SocialButton = SocialLogin(Button)
 
+class GitHubLogin extends Component {
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      githubToken: ''
+    }
+
+    this.trackChange = this.trackChange.bind(this)
+  }
+
+  trackChange (e) {
+    this.setState({
+      githubToken: e.target.value
+    })
+  }
+
+  render () {
+    return (
+      <div style={{ marginTop: '1rem' }}>
+        <h3>GitHub Authentication with personal token</h3>
+        <label htmlFor='gh_token' style={{ display: 'block' }}>GitHub Personal Token</label>
+        <input
+          id='gh_token'
+          type='text'
+          onChange={this.trackChange}
+          value={this.state.githubToken}
+          style={{ display: 'block', marginBottom: '1rem', minWidth: '50%' }}
+        />
+        {
+          this.state.githubToken
+            ? <SocialButton
+              provider='github'
+              appId={this.state.githubToken}
+              onLoginSuccess={handleSocialLogin}
+              onLoginFailure={handleSocialLoginFailure}
+            >
+              Login with GitHub
+            </SocialButton>
+            : null
+        }
+      </div>
+    )
+  }
+}
+
 ReactDOM.render(
   <div>
-    <SocialButton
-      provider='facebook'
-      appId='309479849514684'
-      onLoginSuccess={handleSocialLogin}
-      onLoginFailure={handleSocialLoginFailure}
-    >
-      Login with Facebook
-    </SocialButton>
-    <SocialButton
-      provider='google'
-      appId='1085669919173-lslfngv7lb6j9sr7eostmtk54mrdmhc5.apps.googleusercontent.com'
-      onLoginSuccess={handleSocialLogin}
-      onLoginFailure={handleSocialLoginFailure}
-    >
-      Login with Google
-    </SocialButton>
-    <SocialButton
-      provider='linkedin'
-      appId='81oplz05qxuccs'
-      onLoginSuccess={handleSocialLogin}
-      onLoginFailure={handleSocialLoginFailure}
-    >
-      Login with LinkedIn
-    </SocialButton>
-    <SocialButton
-      autoCleanUri
-      provider='instagram'
-      appId='afdf675d26214280ac9a792afea5651c'
-      redirect='http://localhost:8080'
-      onLoginSuccess={handleSocialLogin}
-      onLoginFailure={handleSocialLoginFailure}
-    >
-      Login with Instagram
-    </SocialButton>
-    <SocialButton
-      provider='github'
-      appId='87c2663f0f2e624f6f15533f15f3015858e39631'
-      onLoginSuccess={handleSocialLogin}
-      onLoginFailure={handleSocialLoginFailure}
-    >
-      Login with GitHub
-    </SocialButton>
+    <div>
+      <SocialButton
+        provider='facebook'
+        appId='309479849514684'
+        onLoginSuccess={handleSocialLogin}
+        onLoginFailure={handleSocialLoginFailure}
+      >
+        Login with Facebook
+      </SocialButton>
+      <SocialButton
+        provider='google'
+        appId='1085669919173-lslfngv7lb6j9sr7eostmtk54mrdmhc5.apps.googleusercontent.com'
+        onLoginSuccess={handleSocialLogin}
+        onLoginFailure={handleSocialLoginFailure}
+      >
+        Login with Google
+      </SocialButton>
+      <SocialButton
+        provider='linkedin'
+        appId='81oplz05qxuccs'
+        onLoginSuccess={handleSocialLogin}
+        onLoginFailure={handleSocialLoginFailure}
+      >
+        Login with LinkedIn
+      </SocialButton>
+      <SocialButton
+        autoCleanUri
+        provider='instagram'
+        appId='afdf675d26214280ac9a792afea5651c'
+        redirect='http://localhost:8080'
+        onLoginSuccess={handleSocialLogin}
+        onLoginFailure={handleSocialLoginFailure}
+      >
+        Login with Instagram
+      </SocialButton>
+    </div>
+    <GitHubLogin />
   </div>,
   document.getElementById('app')
 )
