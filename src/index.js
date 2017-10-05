0cbd423e35b78e4cf8cab96937fb44407417e1cc
Nicolas Goudry <goudry.nicolas@gmail.com>
2017-10-05 22:53:48 +0200

    Fix wrong error message

--- src/index.js
+++ src/index.js
@@ -132,6 +132,8 @@ const SocialLogin = (WrappedComponent) => class SocialLogin extends Component {
       this.sdk.login().then(this.onLoginSuccess, this.onLoginFailure)
     } else if (this.state.isLoaded && this.state.isConnected) {
       this.props.onLoginFailure('User already connected')
+    } else if (this.state.isLoaded && this.state.isFetching) {
+      this.props.onLoginFailure('Fetching user...')
     } else {
       this.props.onLoginFailure('SDK not loaded')
     }
