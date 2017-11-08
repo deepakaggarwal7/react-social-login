782c3111dc8f2e885046dade9e85091e8823ef65
Nicolas Goudry <goudry.nicolas@gmail.com>
2017-11-08 22:09:43 +0100

    Update deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+if ! git diff-index --quiet HEAD --; then
+  echo "Git tree not clean"
+  exit 1
+fi
+
 export FB_APP_ID=530411637301990
 export INSTAGRAM_REDIRECT=https://nicolas-goudry.github.io/react-social-login
 
@@ -17,4 +22,65 @@ if [ $build -ne 0 ]; then
   exit 1
 fi
 
+git add demo/public
+add=$?
 
+if [ $add -ne 0 ]; then
+  exit 1
+fi
+
+git stash
+stash=$?
+
+if [ $stash -ne 0 ]; then
+  exit 1
+fi
+
+git checkout gh-pages
+checkout=$?
+
+if [ $checkout -ne 0 ]; then
+  exit 1
+fi
+
+git pull
+pull=$?
+
+if [ $pull -ne 0 ]; then
+  exit 1
+fi
+
+git stash pop
+pop=$?
+
+if [ $pop -ne 0 ]; then
+  exit 1
+fi
+
+mv demo/public/* .
+mv=$?
+
+if [ $mv -ne 0 ]; then
+  exit 1
+fi
+
+git reset HEAD demo/public/*
+reset=$?
+
+if [ $reset -ne 0 ]; then
+  exit 1
+fi
+
+git add index.html main.js
+add=$?
+
+if [ $add -ne 0 ]; then
+  exit 1
+fi
+
+git commit -m "Update gh-pages"
+commit=$?
+
+if [ $commit -ne 0 ]; then
+  exit 1
+fi
