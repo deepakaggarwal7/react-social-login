67fdb9a665754add9bfbea2343a2dd08bd34e6d7
deepakaggarwal7 <deepakaggarwal7@gmail.com>
2020-08-03 18:13:26 +0530

    types added to dist

---
+++ dist/types/index.d.ts
@@ -0,0 +1,22 @@
+export type Provider = "amazon" | "facebook" | "github" | "google" | "instagram" | "linkedin";
+
+export interface Props {
+  appId: string
+  autoCleanUri?: boolean
+  autoLogin?: boolean
+  gatekeeper?: string
+  getInstance?: () => React.RefObject<any>
+  onLoginFailure?: (result: any) => void
+  onLoginSuccess?: (result: any) => void
+  onLogoutFailure?: (result: any) => void
+  onLogoutSuccess?: (result: any) => void
+  provider: Provider
+  className?: string
+  redirect?: string
+  scope?: [] | string
+  [key: string]: any
+}
+
+export default function SocialLogin(
+  WrappedComponent: React.ComponentType<any>
+): React.ComponentType<Props>
